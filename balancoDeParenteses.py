9fd1903ea339e594016cd7d5822a0e5cdc59aab6
João Victor Macedo <gmvictorr@outlook.com>
2023-09-17 12:19:25 -0300

    inclusão do texto da questão

--- balancoDeParenteses.py
+++ balancoDeParenteses.py
@@ -1,3 +1,27 @@
+"""
+Dada uma expressão qualquer com parênteses, indique se a quantidade de parênteses está correta ou não, sem levar em
+conta o restante da expressão. Por exemplo:
+
+a+(b*c)-2-a        está correto
+(a+b*(2-c)-2+a)*2  está correto
+
+enquanto
+
+(a*b-(2+c)         está incorreto
+2*(3-a))           está incorreto
+)3+b*(2-c)(        está incorreto
+
+Ou seja, todo parênteses que fecha deve ter um outro parênteses que abre correspondente e não pode haver parênteses que
+fecha sem um previo parenteses que abre e a quantidade total de parenteses que abre e fecha deve ser igual.
+
+Entrada
+Como entrada, haverá N expressões (1 <= N <= 10000), cada uma delas com até 1000 caracteres.
+
+Saída
+O arquivo de saída deverá ter a quantidade de linhas correspondente ao arquivo de entrada, cada uma delas contendo as
+palavras correct ou incorrect de acordo com as regras acima fornecidas.
+"""
+
 
 class Stack:
     def __init__(self):
@@ -24,6 +48,7 @@ class Stack:
     def size(self):
         return len(self.items)
 
+
 def balanco_de_parenteses(expression):
 
     parentesesDireito = Stack()
@@ -40,7 +65,6 @@ def balanco_de_parenteses(expression):
         return "correct"
     else:
         return "incorrect"
-
 
 
 while True:
